b345044225a697eaf9f71622dd86478333e4c1c0
Sam Vermette <samvermette@gmail.com>
2012-02-27 19:27:30 -0500

    Update Readme.

--- Demo/Demo/DemoViewController.m
+++ Demo/Demo/DemoViewController.m
@@ -18,7 +18,8 @@
     
     SVHTTPRequest *request = [[SVHTTPRequest alloc] initWithAddress:@"http://github.com/api/v2/json/repos/show/samvermette/SVHTTPRequest"
                                                              method:SVHTTPRequestMethodGET 
-                                                         parameters:nil completion:^(id response, NSError *error) {
+                                                         parameters:nil 
+                                                         completion:^(id response, NSError *error) {
                                                              watchersLabel.text = [NSString stringWithFormat:@"SVHTTPRequest has %@ watchers", [[response valueForKey:@"repository"] valueForKey:@"watchers"]];
                                                          }];
     request.cachePolicy = NSURLRequestReloadIgnoringLocalCacheData;
